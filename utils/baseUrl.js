ba6b8206f3a16f258d450329193b5e6af63af8e3
Numan Naeem <numan.naeem9@gmail.com>
2022-11-16 19:30:10 +0530

    edit baseurl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://numstagram.numxn.me'
+    ? 'https://www.numstagram.numxn.me'
     : 'http://localhost:3000'
 
 module.exports = baseUrl
